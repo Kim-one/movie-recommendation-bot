266fe545b07deb7aef6ac7f981441e61422f49ad
Kim-one <144210806+Kim-one@users.noreply.github.com>
2025-08-24 23:18:59 -0300

    fixed a few display issues

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,56 +1,74 @@
 import '../styles/chatpage.css';
+import '../styles/chatbot.css';
 import {useState} from "react";
 import axios from "axios";
+// import ChatBot from "./chatbot";
 
 const Chat =()=>{
-    const [messages, setMessages] = useState('')
+    const [messages, setMessages] = useState('');
     const [input, setInput] = useState([]);
 
     const handleSubmit = async (e)=>{
         e.preventDefault();
         const newMessage = {user: messages, bot: ''};
-        setInput([...input, newMessage]);
+
+        //user input
+        setInput((prev)=>[...prev, newMessage]);
 
         try{
             const response = await axios.post('http://localhost:5000/chatbot', {messages});
-            const botMessage = response.data.response;
-            newMessage.bot = botMessage;
-            setInput([...input, newMessage]);
+            const botMessage = response.data.message;
+            const recommendations = response.data.recommendations || [];
+
+            newMessage.bot = {message:botMessage, recommendations}
+            console.log(newMessage.bot)
+            // newMessage.bot = botMessage;
+            // setInput((prev)=>[...prev,{user:messages, bot:botMessage}]);
         } catch (error){
             console.error("Error communicating with the backend", error);
         }
 
         setMessages('');
-        // const response = await fetch('http://localhost:5000/chatbot',{
-        //     method:'POST',
-        //     headers:{'Content-Type': 'application/json'},
-        //     body: JSON.stringify({messages:input}),
-        // });
-        // const data = await response.json();
-        // setMessages([...messages,{text:input, from: 'user'}, {text:data.response, from: 'bot'}]);
-        // setInput('');
     };
 
     return (
         <div className={'chat-page'}>
-            <h1 className={'title'}>ChatBot</h1>
+            <h1 className={'title1'}>ChatBot</h1>
             <div>
                 {input.map((msg, index)=>(
-                    <div key={index}>
-                        <p><strong>You:</strong> {msg.user}</p>
-                        <p><strong>Bot:</strong> {msg.bot}</p>
+                    <div className={'msg'} key={index}>
+                        <p className={'userChatBubble'}><strong>You:</strong> {msg.user}</p>
+                        <p className={'botChatBubble'}><strong>Bot:</strong> {msg.bot.message}</p>
+                        {msg.bot.recommendations && msg.bot.recommendations.length >0 &&(
+                            <ul>
+                                {msg.bot.recommendations.map((rec,i)=>(
+                                    <li key={i}>
+                                        <h4>{rec.title}</h4>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </div>
                 ))}
             </div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type={"text"}
-                    value={messages}
-                    onChange={(e) => setMessages(e.target.value)}
-                    placeholder={"Type your message ..."}
-                />
-                <button type={"submit"}>Send</button>
-            </form>
+            {/*<ChatBot></ChatBot>*/}
+            <div className={'chat-container1'}>
+                <div className={`chat-bot`}>
+                    <div className={'chat-img'}></div>
+                    <div className={'chat-input'}>
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type={"text"}
+                                value={messages}
+                                onChange={(e) => setMessages(e.target.value)}
+                                placeholder={"Type your message ..."}
+                                className={'input'}
+                            />
+                            <button type={"submit"}>Send</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
